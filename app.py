bf1dde6c70509c7622591cfd9c06b26645b0f8bf
stannam <stan.nam98@gmail.com>
2022-07-15 17:05:45 -0700

    added more example sentences.

--- app.py
+++ app.py
@@ -8,12 +8,15 @@ app = Flask(__name__)
 
 @app.route('/',  methods=["GET", "POST"])
 def main():
-    # choose a placeholder sentence to appear upon launching
-    sentences = ['고양이는 예쁘다.',
-                 '다람쥐는 점프한다.',
-                 '아버지가 가방에 들어가신다.',
-                 '통사론 재미없어.',
-                 '집에 올 시간이 지났다.']
+    # Upon launching, choose a placeholder sentence to appear. List 'sentences' holds example Korean sentences.
+    sentences = ['고양이는 예쁘다.',    # Cats are pretty.
+                 '다람쥐는 점프한다.',  # The squirrel jumps.
+                 '아버지가 가방에 들어가신다.',   # Father enters the room.
+                 '통사론 재미없어.',    # Syntax is no fun.
+                 '음운론이 최고야',     # Phonology is the best.
+                 '집에 올 시간이 지났다.',   # They should have arrived home already.
+                 '언어학은 재밌어.'     # Linguistics is fun.
+                 ]
     sentence = choice(sentences)
 
     # and process the sentence.
@@ -26,10 +29,7 @@ def main():
 
     try:
         if request.method == "POST":
-            print(f"in request POST")
-            # return redirect(url_for('edit_database', sentence=sentence))
             sentence = request.form['sentence']
-            print(f"sentence: {sentence}")
             results = worker.main(sentence)
             sentence = sentence
             yale = '\t\t'.join(results[1])
